e0048ff08c703de2432fcc90a39862b73fba8880
PeRobin <robinnibor94@gmail.com>
2018-04-13 00:34:44 +0200

    Added like button and comment section

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
     styleUrls: ['entry.component.css']
 })
 export class EntryComponent {
+    
+    title: string = 'My first Angular Photo';
+    photo: string = 'http://placehold.it/600x300?text=Angular Photo'; 
+    description: string = 'My first angular photo description!'; 
+    comments: any[] = [
+        {name: "Robina", comment: "This is a comment"}, 
+        {name: "Kjartan", comment: "This is another comment"}, 
+        {name: "Nils", comment: "This is third comment"}, 
+    ]
 
 }
